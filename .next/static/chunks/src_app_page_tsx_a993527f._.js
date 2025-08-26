9f2eab51d38f393c64916721cd73ba487aeb8c67
farhankhan987 <fk9392625@gmail.com>
2025-08-26 12:02:02 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1756189921691

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_7d376cc9._.js",
+  "static/chunks/node_modules_f7d0edeb._.js"
+],
+    source: "dynamic"
+});
